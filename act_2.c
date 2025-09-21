6032e13204b288fc3a9c37245afb13fef7c90790
Ian Alexis Solis Hernández <ianalexis9137@gmail.com>
2025-09-21 16:16:01 -0600

    Programa actualizado con potencias

--- act_2.c
+++ act_2.c
@@ -7,7 +7,7 @@
 
 #define fo  10
 #define A   12.5
-#define Ar  1.25
+#define Ar  15.2f
 #define M   1000
 #define pi 3.14159
 
@@ -15,21 +15,40 @@ void quantize(float f_array[], int L_bits,char file_name[]);
 
 void main(){
     FILE *signal_file;
-    float xs[M];
+    float sin_x[M];
+    float pot_sin = 0.f;
+    float noise[M];
+    float pot_noise = 0.f;
+    float sin_x_noise[M];
+    float pot_sin_soise = 0.f;
+
     srand((unsigned int)time(NULL));
 
     signal_file = fopen("signal.dat", "w");
 
     for(int n = 0; n < M; ++n){
-        xs[n] = A*sin(2*pi*fo*n/M) + Ar *((float)rand()/(float)RAND_MAX);
-        fprintf(signal_file, "%f \n", xs[n]);
+        sin_x[n] = A*sin(2*pi*fo*n/M);
+        pot_sin += sin_x[n] * sin_x[n];
+        noise[n] = Ar *((float)rand()/(float)RAND_MAX);
+        pot_noise += noise[n] * noise[n];
+        sin_x_noise[n] = sin_x[n] + noise[n];
+        pot_sin_soise += sin_x_noise[n] * sin_x_noise[n];
+        fprintf(signal_file, "%f \n", sin_x_noise[n]);
     }
+
+    pot_sin = (1/(float)M)*pot_sin;
+    pot_noise = (1/(float)M)*pot_noise;
+    pot_sin_soise = (1/(float)M)*pot_sin_soise;
+
+    printf("Potencia de señal sinusoidal: %f\n", pot_sin);
+    printf("Potencia del ruido: %f\n", pot_noise);
+    printf("Potencia de señal sinusoidal con ruido: %f\n", pot_sin_soise);
 
     fclose(signal_file);
 
-    quantize(xs, 5, "q5_signal.dat");
-    quantize(xs, 9, "q9_signal.dat");
-    quantize(xs, 11, "q11_signal.dat");
+    quantize(sin_x, 5, "q5_signal.dat");
+    quantize(sin_x, 9, "q9_signal.dat");
+    quantize(sin_x, 11, "q11_signal.dat");
 
     system("gnuplot -p graf_quant.gp");
 
@@ -40,6 +59,7 @@ void quantize(float f_array[], int L_bits, char file_name[]){
     if(strlen(file_name) > 40) {printf("Longitud de nombre muy grande"); return;}
     int q_array[M] = {};
     int q_max_val = (int)pow(2,L_bits-1)-1;
+    float ECM = 0.f;
     FILE *file;
     file = fopen(file_name, "w");
 
@@ -52,10 +72,12 @@ void quantize(float f_array[], int L_bits, char file_name[]){
             q_array[n] = -q_max_val;
         }
         else{
-            q_array[n] = (int)f_array[n];
+            q_array[n] = (int)(f_array[n]+0.5);
         }
+        ECM += (f_array[n] - (float)q_array[n])*(f_array[n] - (float)q_array[n]);
         fprintf(file, "%d \n", q_array[n]);
     }
     fclose(file);
-
+    ECM = (1/(float)M)*ECM;
+    printf("El error cuadratico medio a %d bits es: %f\n", L_bits, ECM);
 }
--- graf_quant.gp
+++ graf_quant.gp
@@ -1,9 +1,9 @@
 reset
 set grid
-set style data lines
 set autoscale
 
 #------------- Graf 1 ----------------
+set style data lines
 set title "Funcion seno con ruido aleatorio"
 set xlabel "Muestras"
 set ylabel "seno(n) + ruido"
@@ -11,22 +11,23 @@ plot "signal.dat"
 pause -1 "Presiona Enter para continuar..."
 
 #------------- Graf 2 ----------------
+set style data steps
 set title "Señal cuantizada a 5 bits"
 set xlabel "Muestras"
 set ylabel "Señal cuantizada"
-plot "q5_signal.dat"
+plot "q5_signal.dat" with steps title "Nivel 5 bits"
 pause -1 "Presiona Enter para continuar..."
 
 #------------- Graf 3 ----------------
 set title "Señal cuantizada a 9 bits"
 set xlabel "Muestras"
 set ylabel "Señal cuantizada"
-plot "q9_signal.dat"
+plot "q9_signal.dat" with steps title "Nivel 9 bits"
 pause -1 "Presiona Enter para continuar..."
 
 #------------- Graf 4 ----------------
 set title "Señal cuantizada a 11 bits"
 set xlabel "Muestras"
 set ylabel "Señal cuantizada"
-plot "q11_signal.dat"
+plot "q11_signal.dat" with steps title "Nivel 11 bits"
 pause -1 "Fin de gráficas"
